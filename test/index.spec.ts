bb87197483d13f20dfe36d81842dde760a3a43ea
Stefan Thomas <justmoon@members.fsf.org>
2016-10-22 12:10:23 -0700

    test: fix/update tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,20 +1,29 @@
 import sinon = require('sinon')
+import chalk = require('chalk')
 import riverpig = require('../src')
 
 describe('riverpig', function () {
-  describe('namespace', function () {
     it('should namespace a logger', function () {
-      const fakeLogMethod = sinon.spy()
-      const logger = Object.assign({}, console, {
-        log: fakeLogMethod
-      })
-
-      const namespacedLogger = riverpig.namespace('test: ', logger)
-
-      namespacedLogger.log('foo')
-
-      sinon.assert.calledOnce(fakeLogMethod)
-      sinon.assert.calledWith(fakeLogMethod, 'test: foo')
+    const fakeLogMethod = sinon.spy()
+    const logger = Object.assign({}, console, {
+      info: fakeLogMethod
     })
+
+    const fakeClock = sinon.useFakeTimers('Date')
+
+    const namespacedLogger = riverpig('test', { parentLogger: logger })
+
+    namespacedLogger.info('foo')
+
+    fakeClock.restore()
+
+    sinon.assert.calledOnce(fakeLogMethod)
+    sinon.assert.calledWith(
+      fakeLogMethod,
+      chalk.gray('1970-01-01T00:00:00.000Z') + ' ' +
+      chalk.cyan('test') + ' ' +
+      chalk.blue('info') + ' ' +
+      'foo'
+    )
   })
 })
